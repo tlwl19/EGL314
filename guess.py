397459aba95edd36cdd430ff7e3438be093353b1
tlwl19 <115998261+tlwl19@users.noreply.github.com>
2023-01-02 18:27:05 +0800

    Update guess.py

--- guess.py
+++ guess.py
@@ -3,11 +3,9 @@ from tkinter import *   #import tkinter library
 main = Tk()   #Create a main window
 main.title("Guess the Horoscope") #Title will be shown on the GUI window
 
-#Header of the game
-frame0 = Frame(main)
-frame0.grid(row=0, columnspan=3)
-
-headername = 
+#Header for the game
+headername = Label(text="Guess the Horoscope", font=('Arial', 30)) 
+headername.grid(row=0, columnspan=3)
 
 #First frame is created for the 3x4 grid
 frame1 = Frame(main)
@@ -21,25 +19,28 @@ horoscope = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Sc
 button = [[r for r in range(inputrow)] for c in range(inputcolumn)]
 for r in range(inputrow):
     for c in range(inputcolumn):
-        button[c][r] = Button(frame1, text=horoscope[counter], font=('Arial',10), height=5, width=10) #button[c][r] start from column first instead of row 
+        button[c][r] = Button(frame1, text=horoscope[counter], font=('Arial',15), height=5, width=10) #button[c][r] start from column first instead of row 
         button[c][r].grid(row=r, column=c) 
         counter = counter+1
 
 
 #Second frame is created for the button "GUESS!"
 frame2 = Frame(main)
-frame2.grid(row=2, column=1)
+frame2.grid(row=1, column=1)
 
-guessbtn = Button(frame2, text="Guess!!", font=('Arial',20))
+guessbtn = Button(frame2, text="GUESS!!", font=('Arial',20))
 guessbtn.grid(row=0, column=1)
 
 
 #Third frame is created for the 'START/RESET/SCORE' 
 frame3 = Frame(main)
-frame3.grid(row=3, column=2)
+frame3.grid(row=1, column=2)
 
 startbtn = Button(frame3, text="START GAME", font=('Arial', 20))
 startbtn.grid(row=0, column=2)
+
+resetbtn = Button(frame3, text="RESET GAME", font=('Arial', 20))
+resetbtn.grid(row=1, column=2)
 
 
 main.mainloop()  #for the window to stay
