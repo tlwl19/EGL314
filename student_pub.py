ff7af5c5e42c0fbb5c70063ac7d470150a2b0543
tlwl19 <115998261+tlwl19@users.noreply.github.com>
2023-01-03 19:18:27 +0800

    ...

--- student_pub.py
+++ student_pub.py
@@ -1,17 +1,17 @@
-import time
-import paho.mqtt.client as mqtt
+#import time
+#import paho.mqtt.client as mqtt
 
-def on_publish(client, userdata, mid):
-    print("WE LOVE EGL314!")
+#def on_publish(client, userdata, mid):
+#    print("WE LOVE EGL314!")
 
-def pubpic(output):
-    k = output
-    msg = str(k)
-    pubMsg = client.publish(
-        topic='studentpi/team2', #Please change according to your respective Group  
-        payload=msg.encode('utf-8'),
-        qos=0,
-    )
+#def pubpic(output):
+#    k = output
+ #   msg = str(k)
+  #  pubMsg = client.publish(
+   #     topic='studentpi/team2', #Please change according to your respective Group  
+    #    payload=msg.encode('utf-8'),
+     #   qos=0,
+   # )
 
-    pubMsg.wait_for_publish()
-    print(pubMsg.is_published())
+   # pubMsg.wait_for_publish()
+    #print(pubMsg.is_published())
