62ebbc62e5ff21c3bea4884f95fe16512a8c8a6a
BxGxJ <Jerrellngjw@gmail.com>
2023-01-14 18:02:01 +0800

    focus

--- focus.py
+++ focus.py
@@ -1,13 +1,39 @@
 from tkinter import *
-
 from datetime import datetime
+import random
+import cartoon
+from PIL import Image, ImageTk
 
 counter = 0
 running = False
 
+def show_Image(choice):
+    path2 = "icons/" + str(choice) + ".png"
+    myImage = Image.open(path2)
+
+    #sending to cartoon.py
+    #first path refers to input for img
+    #path2 refers to variable
+    cartoon.pixelised(path = path2)
+    myImage = Image.open("cartoon.png")
+
+def click():
+    global numberxlist
+    numberx = random.randint(0,5) #generate a random no.
+    if len(numberxlist) >= 1:
+        while numberx == numberxlist[0]:
+            numberx = random.randint(0,5)
+        else:
+            numberxlist[0] = numberx
+            print(numberx)
+    else:
+        numberxlist = [numberx]
+        print(numberxlist[0])
+    #show_Image(numberx) #send to polariser the number
+
 # start function of the stopwatch
 def Start(label):
-    global running
+    global running, Lno
     running=True
     counter_label(label)
     start['state']='disabled'
@@ -15,14 +41,33 @@ def Start(label):
     reset['state']='normal'
     for i in range (0, 6): 
         btn[i]['state']='normal'
+    level['text']='Level '+ str(Lno)
+    click()
    
 # Stop function of the stopwatch
-def Stop():
-    global running
-    start['state']='normal'
-    stop['state']='disabled'
-    reset['state']='normal'
-    running = False
+def Stop(m):
+    global running, numberxlist, Lno
+    if m == numberxlist[0]:
+        start['state']='disabled'
+        stop['state']='disabled'
+        reset['state']='normal'
+        running = False
+        print('stop')
+        for i in range (0, 6): 
+            btn[i]['state']='disabled'
+        Lno = Lno+1
+    elif m == 10:
+        start['state']='disabled'
+        stop['state']='disabled'
+        reset['state']='normal'
+        running = False
+        print('stop')
+        for i in range (0, 6): 
+            btn[i]['state']='disabled'
+        Lno = 0
+    else:
+        running = True
+        print('wrong')
 
 # Reset function of the stopwatch
 def Reset(label):
@@ -30,7 +75,8 @@ def Reset(label):
     counter=0
    
     # If rest is pressed after pressing stop.
-    if running==False:      
+    if running==False:
+        start['state']='normal'      
         reset['state']='disabled'
         label['text']='Welcome!'
    
@@ -63,9 +109,9 @@ def counter_label(label):
             # Delays by 1000ms=1 seconds and call count again.
             timer.after(1000, count) 
             counter += 1
-            print(counter)
+            #print(counter)
             if counter > 10:
-                Stop()
+                Stop(10)
            
    
     # Triggering the start of the counter.
@@ -78,13 +124,19 @@ main = Tk()
 #Define geometry of the window
 main.geometry("1000x300")     
 
+numberxlist = []
+Lno = 0
+
 #Header for the game
 headername = Label(text="ICONcentrate", font=('Arial', 30)) 
-headername.grid(row=0, columnspan=3)
+headername.grid(row=0, columnspan=4)
 
 #Instructions
 instruction = Label(main, text="1. Press on start game etc...", font=('Arial', 15), bg='pink')
 instruction.grid(row=1, columnspan=4)
+
+level = Label(main, text='Level '+ str(Lno), font=('Arial', 10))
+level.grid(row=2, column=0)
 
 #Timer
 timer = Label(main, text='Timer', font=('Arial', 10))
@@ -104,7 +156,7 @@ icons = ['Facebook', 'Instagram', 'Twitter', 'Youtube', 'Tiktok', 'Netflix']
 btn = [i for i in range(len(icons))]  #defining the number of buttons
 
 for i in range (0, 6):  #Assigning array values into btn 
-    btn[i] = Button(frame1, text=icons[i], state='disabled', command=Stop)
+    btn[i] = Button(frame1, text=icons[i], state='disabled', command=lambda m=i:Stop(m))
     btn[i].grid(row=0, column=i)
 
 
