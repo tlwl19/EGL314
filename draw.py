08ab144f9c0377c646cb4e81b73f49a9fd4e1719
ruien <lim.ruien03@gmail.com>
2023-01-27 18:56:05 +0800

    Deleted files

--- draw.py
+++ draw.py
@@ -1,10 +1,12 @@
 from tkinter import *
 from tkinter import ttk
+import cartoon
 
-# Function to choose colour
+
 def choose_colour(m):
-    global colour #value of the colour selected
+    global colour #Value of the colour selected
     colour = m
+
 
 def colour_picker(r, c):
   global colour
@@ -33,173 +35,67 @@ def colour_picker(r, c):
       button[r][c].config(bg='grey1') 
       value[r][c] = colour
 
+
 def sendbtn():
-  #print(value)
   global value
   print(value)
+  #pubpic(output)
 
-""" def change_colour(m): 
-  global colour
-  colour=m 
-
-  print("colour is {}".format(colour)) """
 
 def allwht():
   for r in range (32):
     for c in range (32):
       button[r][c].config(bg='grey99')
-      value[r][c] = 0
-  #canvasdraw = [[0 for r in range(576)] for c in range(576)]
+      value[r][c] = 0  #This is the angle of grey99
+
 
 def allblk():
   for r in range (32):
     for c in range (32):
       button[r][c].config(bg='grey1')
-      value[r][c] = 90
-  #canvasdraw = [[90 for r in range(576)] for c in range(576)]
-""" 
-def pattern():
-  for j in range (32):
-    for i in range (32):
-      if i == j: 
-        button[i][j].config(bg='grey66')
-        value[i][j] = 40
-      elif i + j == 31: 
-        button[i][j].config(bg='grey66')
-        value[i][j] = 40
-      else:
-        button[i][j].config(bg='grey99')
-        value[i][j] = 0
-
-
-def ramseq():
-  for j in range (32):
-    for i in range (32):
-      if j < 6:
-        button[i][j].config(bg='grey99')
-        value[i][j] = 0
-      elif j >= 6 and j <= 12:
-        button[i][j].config(bg='grey88')
-        value[i][j] = 20
-      elif j >= 12 and j <= 18:
-        button[i][j].config(bg='grey77')
-        value[i][j] = 30
-      elif j >= 18 and j <= 24:
-        button[i][j].config(bg='grey66')
-        value[i][j] = 40
-      elif j >= 24 and j <= 32:
-        button[i][j].config(bg='grey44')
-        value[i][j] = 50  """
+      value[r][c] = 90 #This is the angle of grey1
+ 
 
 def get_x_and_y(event):
-   global lasx, lasy
-   lasx, lasy = event.x, event.y
+   global getx, gety
+   getx, gety = event.x, event.y
 
-def paint(event):
-    global lasx, lasy, value
-    if lasx >= 0 and lasx <= 575 and lasy >= 0 and lasy <= 575:
+
+def paint(event):   #creating line
+    global getx, gety, value
+    if getx >= 0 and getx <= 575 and gety >= 0 and gety <= 575:
       if colour == 0: 
-          canvas.create_line((lasx,lasy, event.x, event.y),fill='grey99',width=4)
-          canvasdraw[lasx][lasy] = 0
+          canvas.create_line((getx,gety, event.x, event.y),fill='grey99',width=4)  #event.x is the position of the mouse relative to the widget
+          canvasdraw[getx][gety] = 0   #store position in canvasdraw
       elif colour == 20:
-          canvas.create_line((lasx,lasy, event.x, event.y),fill='grey88',width=4)
-          canvasdraw[lasx][lasy] = 20
+          canvas.create_line((getx,gety, event.x, event.y),fill='grey88',width=4)
+          canvasdraw[getx][gety] = 20
       elif colour == 30:
-          canvas.create_line((lasx,lasy, event.x, event.y),fill='grey77',width=4)
-          canvasdraw[lasx][lasy] = 30
+          canvas.create_line((getx,gety, event.x, event.y),fill='grey77',width=4)
+          canvasdraw[getx][gety] = 30
       elif colour == 40: 
-          canvas.create_line((lasx,lasy, event.x, event.y),fill='grey66',width=4)
-          canvasdraw[lasx][lasy] = 40
+          canvas.create_line((getx,gety, event.x, event.y),fill='grey66',width=4)
+          canvasdraw[getx][gety] = 40
       elif colour == 50:
-          canvas.create_line((lasx,lasy, event.x, event.y),fill='grey44',width=4)
-          canvasdraw[lasx][lasy] = 50
+          canvas.create_line((getx,gety, event.x, event.y),fill='grey44',width=4)
+          canvasdraw[getx][gety] = 50
       elif colour == 60: 
-          canvas.create_line((lasx,lasy, event.x, event.y),fill='grey33',width=4)
-          canvasdraw[lasx][lasy] = 60
+          canvas.create_line((getx,gety, event.x, event.y),fill='grey33',width=4)
+          canvasdraw[getx][gety] = 60
       elif colour == 70:
-          canvas.create_line((lasx,lasy, event.x, event.y),fill='grey22',width=4)
-          canvasdraw[lasx][lasy] = 70
+          canvas.create_line((getx,gety, event.x, event.y),fill='grey22',width=4)
+          canvasdraw[getx][gety] = 70
       else: 
-          canvas.create_line((lasx,lasy, event.x, event.y),fill='grey11',width=4)
-          canvasdraw[lasx][lasy] = 90
+          canvas.create_line((getx,gety, event.x, event.y),fill='grey11',width=4)
+          canvasdraw[getx][gety] = 90
       get_x_and_y(event)
 
-""" def scaledown(r, c):
-  global lasx, lasy
-  list = []
-  for x in range(r, c):
-    for y in range(r, c):
-      list.append(canvasdraw[x][y])
-  return list """
 
-# def most_frequent(List):
-#   counter = 0
-#   num = List[0]
-     
-#   for i in List:
-#     curr_frequency = List.count(i)
-#     if(curr_frequency> counter):
-#       counter = curr_frequency
-#       num = i
- 
-#   return num
-
-# def store():
-#   List = []
-#   for x in range(0, 17):
-#     List.append(most_frequent(18*x))
-#   #print(List)
-
-""" def save_img():
-  savelist = []
-  #for _ in range(32):
-  savelist.append(function0())
-  print(len(savelist), savelist)
-
-def function0():
-  global newlist, freqlist
-  freqlist = []
-  newlist = []
-  # for x in range(1, 33):
-  #   function1(x)
-  for x in range(1, 33):
-    function1(x)
-  newlist = function1(x)
-  return newlist
-  #print(function1(x))
-
-def function1(n):
-  num1 = 18*(n-1)
-  num2 = num1 + 18
-  list32 = [] #store all the value of 18x18 
-  #for x in range(num1, num2):
-  for x in range(num1, num2):
-    for y in range(18):
-      list32.append(canvasdraw[y][x])
-  #print(len(list32), num1, num2)
-  return most_frequent(list32)
-  #print(len(list32))
-
-def most_frequent(List):
-  global freqlist
-  freq = max(set(List), key = List.count)
-  freqlist.append(freq)
-  return freqlist
-  #return freqlist
-  #print(freqlist)
-  #function3(freqlist)
-  #return function3(num)
-
-#def function3(x):
-  #global newlist
-  #newlist.append(x)
-  #print(newlist) """
-
-def save_draw_colour(list):
+def save_draw_colour(list):  #number convert to colour
   global value
-  for r in range(32):
+  for r in range(32):    
     for c in range(32):
-      if list[r][c] == 0:
+      if list[r][c] == 0: 
         button[c][r].config(bg='grey99')
         value[c][r] = 0
       elif list[r][c] == 20: 
@@ -224,57 +120,30 @@ def save_draw_colour(list):
         button[c][r].config(bg='grey1')
         value[c][r] = 90
 
-def savecanvas():
-  global list2, list1, value, list4
-  list2 = []
-  list1 = []
-  list3 = []
-  list4 = []
-  #f0(18)
+
+def savecanvas():  #convert 576x576 to 32x32 grid
+  global value, list4 
+  list3 = []  #one row only (1x18)
+  list4 = []  #final output of 32x32 array that will convert to grid
   for r in range(0, 576, 18):
-    list3 = []
-    for c in range(0 , 576, 18):
-      #list3 = []
-      #f0(t, i)
-      getnumber = f0(r, c)
-      #print(i, t)
-      list3.append(getnumber)
-    list4.append(list3)
-    #list3.append(list1)
+    list3 = []   #it will be empty once it is done getting the value for list4, will keep repeating 
+    for c in range(0 , 576, 18): #will only loop 32times that is 1x18
+      getnumber = f0(r, c)    #returning only 1 value
+      list3.append(getnumber) #store that 1 value for 32 times
+    list4.append(list3)       #row will loop 32 times
   save_draw_colour(list4)
   print(list4)
   
+
 def f0(x, y): #get the starting x, y of a 18x18 and to return 1 value back to rep the 18x18, to scale down a 18x18 to a 1x1
-  global list0, list1, list2
-  list0 = []
-  #num1 = 18*(x-1)
-  #num2 = num1 + 18
+  global list0
+  list0 = []  #To store the value of 18x18 to store in 1 array,
   for r in range(x ,18+x):
     for c in range(y ,18+y):
       list0.append(canvasdraw[r][c])
   freq = min(set(list0), key = list0.count) #using min instead cause if max almost everytime will get 0,harder for the draw to show; once the 18x18 grid got 1 value change, then return that value
-                                            #Link: https://www.geeksforgeeks.org/python-find-most-frequent-element-in-a-list/
-  #list1.append(freq)
-  return freq 
+  return freq  #Returning min value 
 
-""" def f1():
-  global freq0, list0
-  freq0 = 0
-  freq = max(set(list0), key = list0.count)
-  freq0 = freq
-  return freq0
-
-def f2():
-  global freq0, list1
-  list1 = []
-  #list1 = [f1() for _ in range(32)]
-  list1.append(f1() for _ in range(32))
-  #print(len(list1))
-  #list1.append(freq0)
-
-def f3():
-  global list1, list2
-  list2.append(list1) """
 
 def clearbtn():
   allwht()
@@ -283,121 +152,88 @@ def clearbtn():
       canvasdraw[r][c] = 0
   canvas.delete('all')
   print(canvasdraw)
-  
-def tictactoe():
-  frame2.destroy()
-  frame3.destroy()
-  frame4.destroy()
 
-# def save_img():
-#   x = 18
-#   for y in range(0, 32):
-#     scaledown(y*x, (y*x)+(x-1))
 
 main = Tk()
-main.title("still doing")
+main.title("Draw")
 
-notebook = ttk.Notebook(main) #widget that manages a collection of windows/displays
+master = ttk.Notebook(main) #widget that manages a collection of windows/displays
 
-tab1 = Frame(notebook) #new frame for tab 1
-tab2 = Frame(notebook) #new frame for tab 2
-tab3 = Frame(notebook) #new frame for tab 3
+tabgrid = Frame(master) #new frame for tab grid
+tabdraw = Frame(master) #new frame for tab draw
 
-notebook.add(tab1,text="Grid")
-notebook.add(tab2,text="Draw")
-# notebook.add(tab3, text="tictactoe")
-notebook.grid(row=0, column = 0)
+#Placement is at the top left corner
+master.add(tabgrid,text="Grid")
+master.add(tabdraw,text="Draw")
+master.grid(row=0, column = 0)
 
-frame1 = Frame(tab1, width=800, height=800) #3x3 btn
-frame1.grid(row=0, column=0)
-# frame1.grid(ipadx='64px', ipady='64px')
+gridframe = Frame(tabgrid, width=800, height=800) #32x32 btn
+gridframe.grid(row=0, column=0)
 
-frame2 = Frame(main) #shades btn
-frame2.grid(row=0, column=1)
+shadeframe = Frame(main) #shades btn
+shadeframe.grid(row=0, column=1)
 
-frame3 = Frame(main)
-frame3.grid(row=1, columnspan=2) #colour btns 
+colourframe = Frame(main)
+colourframe.grid(row=1, column=0) #all white/black btns and send btn 
 
-frame4 = Frame(main)
-frame4.grid(row=2, columnspan=2) #send btn
 
-frame5 = Frame(tab2) # draw canvas 
-frame5.grid(row=0, column=0) 
-
-# frame6 = Frame(tab3) # tictactoe
-# frame6.grid(row=0, column=0)
-
-canvas = Canvas(tab2, width=576, height=576, bg='white')  
+#Set canvas background colour to white
+canvas = Canvas(tabdraw, width=576, height=576, bg='white')  
 canvas.grid(row=0, column=0)
 
 canvas.bind('<Button-1>', get_x_and_y)
 canvas.bind('<B1-Motion>',paint)
 canvas.bind('<Enter>', get_x_and_y)
 
-#this variable to store the colour choice 
+#This variable to store the colour choice 
 colour = 0
 canvasdraw = [[0 for r in range(576)] for c in range(576)]  # save eventxy into an array 
-#print(canvasdraw)
-
-#3x3 buttons for tic tac toe
-# button6 = [[j for j in range(3)] for i in range(3)]
-# value = [[0 for i in range(3)] for j in range(3)]
-
-# for j in range (3):
-#   for i in range (3):
-#     button6[i][j] = Button(frame6, font=("Calibri, 23"), width=10, height=5, bg='white', command=lambda r=i, c=j:whitebtn(r, c))
-#     button6[i][j].grid(row=i, column=j)
-
 
 # 32x32 grid
 button = [[r for r in range(32)] for c in range(32)]
-
-value = [[0 for r in range(32)] for c in range(32)]
-# print("Value is {}".format(value))
+value = [[0 for r in range(32)] for c in range(32)]  #angle
 
 for r in range (32):
   for c in range (32):
-    button[r][c] = Button(frame1, font=("Calibri, 5"), width=1, height=1, bg='white', command=lambda x=r, y=c:colour_picker(x, y))
+    button[r][c] = Button(gridframe, font=("Calibri, 5"), width=1, height=1, bg='white', command=lambda x=r, y=c:colour_picker(x, y))
     button[r][c].grid(row=r, column=c)
 
-
 #shades button
-white = Button(frame2, text="White", font=("Calibri, 10"), bg='grey99', width=13, height=2, command=lambda m=0:choose_colour(m))
+white = Button(shadeframe, text="White", font=("Calibri, 10"), bg='grey99', width=13, height=2, command=lambda m=0:choose_colour(m))
 white.grid(row=1, column=0)
-grey1 = Button(frame2, text="Grey1", font=("Calibri, 10"), bg='grey88', width=13, height=2, command=lambda m=20:choose_colour(m))
+grey1 = Button(shadeframe, text="Grey1", font=("Calibri, 10"), bg='grey88', width=13, height=2, command=lambda m=20:choose_colour(m))
 grey1.grid(row=2, column=0)
-grey2 = Button(frame2, text="Grey2", font=("Calibri, 10"), bg='grey77', width=13, height=2, command=lambda m=30:choose_colour(m))
+grey2 = Button(shadeframe, text="Grey2", font=("Calibri, 10"), bg='grey77', width=13, height=2, command=lambda m=30:choose_colour(m))
 grey2.grid(row=3, column=0)
-grey3 = Button(frame2, text="Grey3", font=("Calibri, 10"), bg='grey66', width=13, height=2, command=lambda m=40:choose_colour(m))
+grey3 = Button(shadeframe, text="Grey3", font=("Calibri, 10"), bg='grey66', width=13, height=2, command=lambda m=40:choose_colour(m))
 grey3.grid(row=4, column=0)
-grey4 = Button(frame2, text="Grey4", font=("Calibri, 10"), bg='grey44', width=13, height=2, command=lambda m=50:choose_colour(m))
+grey4 = Button(shadeframe, text="Grey4", font=("Calibri, 10"), bg='grey44', width=13, height=2, command=lambda m=50:choose_colour(m))
 grey4.grid(row=5, column=0)
-grey5 = Button(frame2, text="Grey5", font=("Calibri, 10"), bg='grey33', fg='white', width=13, height=2, command=lambda m=60:choose_colour(m))
+grey5 = Button(shadeframe, text="Grey5", font=("Calibri, 10"), bg='grey33', fg='white', width=13, height=2, command=lambda m=60:choose_colour(m))
 grey5.grid(row=6, column=0)
-grey6 = Button(frame2, text="Grey6", font=("Calibri, 10"), bg='grey11', fg='white', width=13, height=2, command=lambda m=70:choose_colour(m))
+grey6 = Button(shadeframe, text="Grey6", font=("Calibri, 10"), bg='grey11', fg='white', width=13, height=2, command=lambda m=70:choose_colour(m))
 grey6.grid(row=7, column=0)
-black = Button(frame2, text="Black", font=("Calibri, 10"), bg='grey1', fg='white', width=13, height=2, command=lambda m=90:choose_colour(m))
+black = Button(shadeframe, text="Black", font=("Calibri, 10"), bg='grey1', fg='white', width=13, height=2, command=lambda m=90:choose_colour(m))
 black.grid(row=8, column=0)
 
 #save button
-savebtn = Button(frame2, text="Save", font=("Calibri, 10"), bg='light blue', fg='black', width=13, height=2, command=savecanvas)
+savebtn = Button(shadeframe, text="Save", font=("Calibri, 10"), bg='light blue', fg='black', width=13, height=2, command=savecanvas)
 savebtn.grid(row=9, column=0)
 
 #colour button
-allwhite = Button(frame3, text="All White",font=("Calibri, 12"), bg='white', width=13, height=2, command=allwht)
+allwhite = Button(colourframe, text="All White",font=("Calibri, 12"), bg='white', width=13, height=2, command=allwht)
 allwhite.grid(row=0, column=0)
 
-allblack = Button(frame3, text="All Black",font=("Calibri, 12"), bg='black', fg='white', width=13, height=2, command=allblk)
+allblack = Button(colourframe, text="All Black",font=("Calibri, 12"), bg='black', fg='white', width=13, height=2, command=allblk)
 allblack.grid(row=0, column=1)
 
-clear = Button(frame3, text="Clear",font=("Calibri, 12"), bg='gold', width=13, height=2, command=clearbtn)
+clear = Button(colourframe, text="Clear",font=("Calibri, 12"), bg='gold', width=13, height=2, command=clearbtn)
 clear.grid(row=0, column=2)
 
-# 
-
-
 #send btn
-send = Button(frame4, text="Send Image!", font=("Calibri, 12"), width=13, height=2, command=lambda :sendbtn())
-send.grid(row=0, column=0)
+send = Button(colourframe, text="Send Image!", font=("Calibri, 12"), width=13, height=2, command=lambda :sendbtn())
+send.grid(row=0, column=3)
+
+
 
 main.mainloop()
