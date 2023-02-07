085da2d39f22cc57057c9c2286fca9a3884a31e8
ruien <lim.ruien03@gmail.com>
2023-02-07 17:39:29 +0800

    test icon

---
+++ testicons.py
@@ -0,0 +1,26 @@
+from tkinter import *
+from datetime import datetime
+import random
+import cartoon
+from PIL import Image, ImageTk
+from tkinter import messagebox, simpledialog
+
+def show_Image(choice):
+    global icons
+    icons=['nissan']
+    path2 = "car/" + str(icons[choice]) + ".png"
+    myImage = Image.open(path2)
+
+    #sending to cartoon.py
+    #first path refers to input for img
+    #path2 refers to variable
+    cartoon.pixelised(pathway = path2)
+    myImage = Image.open("kawaii.png")
+
+
+show_Image(0)
+
+#rec spiderman ironman fila hawkeye capame blackpanther antman
+#half wintersoldier
+#not hulk
+#cmi thor loki
